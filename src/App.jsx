67b9f80d58e49821045915ab411359c582b5640d
deepubishtcrypto <ib811639@gmail.com>
2025-06-15 08:41:46 +0530

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center text-2xl font-bold text-blue-600 mt-10">
+      Hello World from React + Tailwind + Vite!
+    </div>
+  );
+}
+
+export default App;
